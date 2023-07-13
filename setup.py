e0172eaa2e87a143ecc1e739bda94af9d7c58cb7
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2023-07-13 17:09:46 +0100

    Upgrade Pillow to 10.0.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 requirements = [
-    'Pillow==9.3.0'
+    'Pillow==10.0.0'
 ]
 
 test_requirements = [
